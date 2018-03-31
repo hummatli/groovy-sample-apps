883e33626bc27ac2cc4567078e5d42ba386281be
Sattar Hummatli <settarxan@gmail.com>
2018-03-31 08:03:32 +0400

    try groovy builders

---
+++ samples/HelloWorld_IntelliJ/src/com/hummatli/collections.groovy
@@ -0,0 +1,12 @@
+package com.hummatli
+
+
+def funWithMap(map){
+    println map
+}
+
+funWithMap(a:"ds")
+funWithMap a:"ds"
+funWithMap([a:"ds"])
+funWithMap(a:"ds", b:"sds")
+funWithMap a:"ds", b:"sds"
--- samples/HelloWorld_IntelliJ/src/com/hummatli/functions.groovy
+++ samples/HelloWorld_IntelliJ/src/com/hummatli/functions.groovy
@@ -12,3 +12,25 @@ def f2() {
 }
 
 println f2()
+
+
+def f3(text) {
+    println text
+}
+
+f3("Call with parantheses")
+f3 "call without paranthes"
+
+def f4(text1, text2) {
+    println text1
+}
+
+f4("Call with parantheses", "txt2")
+f4 "call without paranthes", "txt2"
+
+
+def f5(arg1, arg2, arg3) {
+    
+}
+
+f5 "", "", {}
